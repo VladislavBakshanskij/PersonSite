fe6c5e17a2cba0a4f4af83fd8dc75a062ddb816a
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2020-08-26 12:52:55 +0300

    Initial

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Content from '../Content/Content';
+import Footer from '../Footer/Footer';
+
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+  );
+}
+
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function Footer() {
+    return (
+        <div> Hello world from Footer! </div>
+    );
+}
