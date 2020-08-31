eceae0e709943d916938cdc2b5ab11923c6fc489
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2020-08-31 08:50:47 +0300

    Test automatic deployment

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     return (
         <main>
             <div className="container" style={{marginTop: 25, marginBottom: 25, bottom: "100%"}}>
-                <h3>Not found 404<em>:(</em></h3>
+                <h3>Сорян, но ресурс не был найден <em>:(</em></h3>
             </div>
         </main>
     );
